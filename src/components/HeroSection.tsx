3c5019bc3eae4b8dd8f36c71f55239f3900bff7a
FaStY661 <kmilohranov@mail.ru>
2025-07-01 01:43:42 +0300

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,8 +13,8 @@ const HeroSection = () => {
       <div className="absolute inset-0 bg-black/40"></div>
       <div className="relative z-10 text-center px-6">
         <h1 className="text-5xl md:text-7xl font-bold mb-6">
-          <span className="text-red">Chaya</span>
-          <span className="text-white">Tattoo</span>{" "}
+          <span className="text-red"> CHAYA </span>
+          <span className="text-white"> TATTOO </span>{" "}
         </h1>
         <p className="text-xl md:text-2xl text-gray-200 mb-12 max-w-3xl mx-auto">
           Превратите ваше видение в потрясающее искусство на теле
