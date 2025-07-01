79394d98ea9e64bb509cb72ac0ac6e0d66f30539
FaStY661 <kmilohranov@mail.ru>
2025-07-01 13:23:37 +0300

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -10,9 +10,7 @@ const AboutSection = () => {
         </h2>
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           <div className="text-center lg:text-left">
-            <img 
-              src="polina.jpg"
-              alt="Мастер Полина"
+            <img src="polina.jpg"
               className="w-80 h-80 rounded-full mx-auto lg:mx-0 mb-8 object-cover border-4 border-black"
             />
           </div>
